d2672986560c7a0bc6dc4f7c89362b35193503f7
Laura Nadolski <laura.nadolski@gmail.com>
2018-01-20 17:35:58 +0000

    automatically backed up by learn

--- dom.js
+++ dom.js
@@ -2,7 +2,7 @@ var myDocument = [
  // to get the object out of the first array index = myDocument[0].children[0].children
  // myDocument[0].children[0].children returns an array. If I wanted to grab the value key of the first object in this array.
  var listItems = myDocument[0].children[0].children
- //
+ // To get the first item out, 
  // We can drill down into the nested DOM using tools we already have.
  
  
