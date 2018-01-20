9c672b6f163960b926ba0d44a599f529bcf9a23b
Laura Nadolski <laura.nadolski@gmail.com>
2018-01-20 17:34:07 +0000

    automatically backed up by learn

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
 var myDocument = [
- // to get the object out of the first array index = myDocument[0].children
+ // to get the object out of the first array index = myDocument[0].children[0].children
  // We can drill down into the nested DOM using tools we already have.
  
  
